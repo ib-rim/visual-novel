26fa0230d8cf7899d0af93a90592e973128a3f4b
ib-rim <ib.bashir@hotmail.com>
2023-06-13 18:53:19 +0100

    Add example scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './reset.css';
+import Character from './Character';
+
+import Dialogue from './components/Dialogue/Dialogue';
+import Scene from './components/Scene/Scene';
+
+import scene1bg from './assets/img/drydock.png';
+
+import grayfaceHappy from './assets/img/grayface-happy.png';
+import grayfaceSad from './assets/img/grayface-sad.png';
+import grayfaceNeutral from './assets/img/grayface-neutral.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const grayface = new Character("Gray Face", grayfaceHappy, grayfaceSad, grayfaceNeutral);
+
+    const startPuzzle = () => {
+        alert("Puzzle started");
+    }
+
+    const scene1Events = [
+        <Dialogue character={grayface} mood={"neutral"}>
+            First, I'm neutral.
+        </Dialogue>,
+        <Dialogue character={grayface} mood={"happy"}>
+            Secondly, I'm happy.
+        </Dialogue>,
+        <Dialogue character={grayface} mood={"sad"}>
+            Thirdly, I'm sad.
+        </Dialogue>,
+        startPuzzle,
+    ];
+
+    return (
+        <div className="App">
+            <Scene dialogue={scene1Events} background={scene1bg}/>
+        </div>
+    );
 }
 
 export default App;
